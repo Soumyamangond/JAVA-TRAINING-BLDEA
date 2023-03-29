670f731b60ea8778a84fec0dbf7768ab30039103
Satis <Satis@KBMANGOND>
2023-03-29 22:20:28 +0530

    commit3

---
+++ src/Test3.java
@@ -0,0 +1,12 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String s1="Ravana";
+	    	String s2="Sita";
+	    	s1=s1.concat(s2);
+	    	System.out.println(s1);
+	}
+
+}
