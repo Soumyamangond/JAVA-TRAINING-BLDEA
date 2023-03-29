e196e85e27166a0e9645bcf7cc5b8e71ea4a67be
Satis <Satis@KBMANGOND>
2023-03-29 22:06:43 +0530

    commit2

---
+++ src/java_training_bldea/Test2.java
@@ -0,0 +1,14 @@
+package java_training_bldea;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<=5) {
+			System.out.println("Good Morning");
+			i++;
+		}
+	}
+
+}
