50d1436ac4b9aac5414954fa891dc74d6e89f1d4
Satis <Satis@KBMANGOND>
2023-03-30 11:36:03 +0530

    commit6

---
+++ src/java_training_bldea/Text6.java
@@ -0,0 +1,13 @@
+package java_training_bldea;
+
+public class Text6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+	       for(int i=1;i<=5;i++) {
+	    	   System.out.println("@");
+	       }
+	}
+
+}
