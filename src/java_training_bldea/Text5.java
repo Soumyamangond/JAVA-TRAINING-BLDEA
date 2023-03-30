9fd71d61b0bc5e89d44346ab6e1770c52e90ba9f
Satis <Satis@KBMANGOND>
2023-03-30 09:12:28 +0530

    commit5

---
+++ src/java_training_bldea/Text5.java
@@ -0,0 +1,15 @@
+package java_training_bldea;
+
+public class Text5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+	    	  for(int j=1;j<=5;j++) {
+	    		  System.out.println(j+" ");
+	    	  }
+	    	 System.out.println();
+	      }
+	}
+
+}
