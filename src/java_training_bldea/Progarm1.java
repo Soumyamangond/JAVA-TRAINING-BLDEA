c42384da655d9b229dad666c5490237f915653b1
Satis <Satis@KBMANGOND>
2023-04-01 13:34:00 +0530

    commit 10

---
+++ src/java_training_bldea/Progarm1.java
@@ -0,0 +1,21 @@
+package java_training_bldea;
+
+import java.util.Scanner;
+
+public class Progarm1 {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc=new Scanner(System.in);
+		System.out.println("enetr the value of n:");
+	         int n=sc.nextInt();
+	         for(int i=1;i<=n;i++) {
+	         for(int j=1;j<=i;j++) {
+	       System.out.print("$");
+	        
+	         }
+	      System.out.println("");
+	         
+	         }
+	}
+	}
