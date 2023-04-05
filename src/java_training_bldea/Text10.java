81522625ea8ae2ea10602da1eb18c9783fa7b3bc
Satis <Satis@KBMANGOND>
2023-04-05 11:08:14 +0530

    constructor

---
+++ src/java_training_bldea/Text10.java
@@ -0,0 +1,30 @@
+package java_training_bldea;
+
+		// TODO Auto-generated method stub
+		class Animal extends Object{
+			Animal(){
+				super();
+				System.out.println("inside zero-parameterized constructer");
+			}
+
+			Animal(int a){
+				super();
+				System.out.println("inside parameterized constructer");
+			}
+		}
+		class Nayi extends Animal{
+			Nayi(){
+				super(10);
+			}
+		}
+
+		public class Text10{
+
+			public static void main(String[] args) {
+				Nayi d1=new Nayi();	
+
+			}
+		}
+
+
+
