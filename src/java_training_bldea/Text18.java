74937f0e7b319c65afe5ed3f5c16828061bb4b86
Satis <Satis@KBMANGOND>
2023-04-06 19:41:02 +0530

    i love my india

---
+++ src/java_training_bldea/Text18.java
@@ -0,0 +1,146 @@
+package java_training_bldea;
+import java.util.Scanner;
+
+public class Text18{
+
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the value of n:");
+			int n=sc.nextInt();
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					if(i==0 || i==(n-1) || j==(n/2)) {
+						System.out.print("* ");
+					}
+					else {
+						
+					System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				
+				
+				
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(j==0 || i==(n-1)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(i==0 && j!=0 && j!=(n-1) || i==(n-1) && j!=0 && j!=(n-1) || j==0 && i!=0 && i!=(n-1) || j==(n-1) && i!=0 && i!=(n-1)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(j==0 && i<=(n/2) || j==(n-1) && i<=(n/2) || (i+j)==(n-1)+(n/2) || (i-j)==(n/2)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				 
+				
+				for(int j=0;j<n;j++) {
+					if(j==0 || i==0 || i==(n/2) || i==(n-1)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("   ");
+				
+				
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(j==0 || i==j && i<=(n/2)|| (i+j)==(n-1) && i<=(n/2) || j==(n-1)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if( i==j && i<=(n/2)|| (i+j)==(n-1) && i<=(n/2) || j==(n/2) && i>=(n/2)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("   ");
+				
+				
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(j==0 || i==j && i<=(n/2)|| (i+j)==(n-1) && i<=(n/2) || j==(n-1)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(i==0 && j!=0 && j!=(n-1) || i==(n-1) && j!=0 && j!=(n-1) || j==0 && i!=0 && i!=(n-1) || j==(n-1) && i!=0 && i!=(n-1)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(i==0 || j==(n/2)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(j==0 || j==(n-1) || i==(n/2)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("  ");
+				for(int j=0;j<n;j++) {
+					if(i==0 && j<=(n-2)|| j==0 || i==(n/2) && j<=(n-2) || i==(n-1) && j<=(n-2)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.print("");
+				for(int j=0;j<n;j++) {
+					if( j==0 || i==0  && j<(n-1) || j==(n-1) && i!=0 && i!=(n/2) || i==(n/2) && j<(n-1)) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				
+				System.out.println("");
+			}
+
+
+		}
+}
